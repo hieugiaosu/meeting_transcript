a59ad23a785ff0662939c314b66eb93d1348f296
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-05-28 11:36:16 +0700

    remove log in console

--- server/src/utils/whisperInference.py
+++ server/src/utils/whisperInference.py
@@ -10,7 +10,6 @@ class WhisperInference:
         self.model.eval()
     def __call__(self,audio,sample_rate):
         audio = torch.tensor(audio)
-        torchaudio.save("test.wav", audio.unsqueeze(0), sample_rate)
         if sample_rate != 16000:
             audio = torchaudio.functional.resample(audio,sample_rate,16000)
         audio = audio.squeeze()
